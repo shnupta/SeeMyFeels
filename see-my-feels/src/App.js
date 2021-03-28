dffe685e7f9c3846ef449ca89620f826d7a44ed3
moraru210 <alexandru210moraru@gmail.com>
2021-03-28 11:05:52 +0100

    written POST request

--- see-my-feels/src/App.js
+++ see-my-feels/src/App.js
@@ -1,17 +1,21 @@
 import logo from './assets/in-my-feels.gif';
 import './App.css';
 import {FileUpload} from './components/Upload'
+import {Credits} from './components/Credits'
+import { FaMusic } from "@react-icons/all-files/fa/FaMusic";
 
 function App() {
   return (
     <div className="App">
       <div className="Title">
+        <FaMusic style={{transform:"translateY(70%"}} />
         <h1>See My Feels</h1>
       </div>
-      <FileUpload />
       <div className="Drake">
-        <img src={logo} alt="still loading..."/>
+        <img src={logo} alt="still loading..." />
       </div>
+      <FileUpload />
+      <Credits />
     </div>
   );
 }
