a46990d97dbb4f381e8ed2968ed3c44bcbcdd2ca
Dehon, Jules <jules.dehon19@imperial.ac.uk>
2021-04-08 10:04:24 +0100

    Create nice front end landing page for webapp
    Decided not to use React, as the integration with flask and the learning curve
    did not seem worth it for quite a simple, static front end. Instead just used
    pure html, css and js.

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,1 +1,27 @@
-console.log("Hello!!");
+// To show overlay when 'generate' button pressed
+
+let processForm = document.getElementById("process-audio");
+let overlay = document.getElementById("loading-overlay");
+processForm.onsubmit = function() {
+  overlay.style.display = "block";
+}
+
+// To change context of audio upload button when file has been uploaded
+
+let audioFileInput = document.getElementById("audio-file-input");
+let audioInputBtn = document.getElementsByClassName("upload-btn")[0];
+
+function uploadClicked() {
+  document.body.onfocus = checkFileSelected;
+}
+
+// Function to check if
+// the user failed to upload file
+function checkFileSelected() {
+  setTimeout(function () {
+    if (audioFileInput.files.length) {
+      audioInputBtn.firstChild.innerHTML = "File Selected";
+    }
+    document.body.onfocus = null;
+  }, 500);
+}
